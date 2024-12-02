ee733456c77395a7f7b3cce10ccf2982d0dd1d4a
Iago71 <iago_trindade2003@hotmail.com>
2024-12-02 07:25:37 +0000

    mudança de sistema

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,16 +6,24 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+ char cidade1[50], cidade2[50], cidade3[50], cidade4[50], cidade5[50], cidade6[50]; 
+ int populaçao,
+
+// Mensagem de Apresentação do Jogo Trunfo
+
+printf("Olá, Seja Bem-Vindo ao Super Truco com Países");
+printf("Escolha uma cidade ilustrada:\n");
+
+printf("A01, A02, B01, B02\n");
+scanf("%s", &cidade1);
+
+
+printf("Informe a População");
+printf("Informe a Área da");
+printf("Informe o PIB da");
+printf("Informe o Número de Pontos Turísticos da");
+
 
     return 0;
 }
