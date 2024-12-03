85e9a060a4101186948f249064452d223f1e4fcd
Iago71 <iago_trindade2003@hotmail.com>
2024-12-03 07:40:09 +0000

    Atualizado por min

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,34 +7,48 @@
 
 int main() {
 
- char cidade1[20], cidade2[20], cidade3[20], cidade4[20], cidade5[20], cidade6[20]; 
+ char cidade1[50], estado[50], codigodaCarta[50], cidade4[20], cidade5[20], cidade6[20]; 
  int pop, pontosturisticos;
  float area, pib;
 
 /* Mensagem de Apresentação do Jogo Trunfo*/
 printf("Olá, Seja Bem-Vindo ao Super Truco com Países\n");
-printf("Escolha uma cidade abaixo:\n");
+printf("Escolha um estado abaixo:\n");
+printf("A. , B , C , D \n");
+scanf("%s", &estado);
 
 /* Aqui será definido a cidade a ser Criada*/
-printf("A01, A02, B01, B02\n");
+printf("Agora Escolha uma cidade:\n");
 scanf("%s", &cidade1);
 
 /* Aqui será definido as Propriedadas da Cidade Escolhida*/
+printf("\n");
 printf("Agora vamos definir as propriedades da cidade: %s\n", &cidade1);
-printf("Defina a População\n");
+
+printf("Defina a População -\n");
 scanf("%d", &pop);
-printf("Defina a Área\n");
+printf("Defina a Área -\n");
 scanf("%f", &area);
-printf("Defina o PIB\n");
+printf("Defina o PIB -\n");
 scanf("%f", &pib);
 printf("Defina o Número de Pontos Turísticos\n");
 scanf("%d", &pontosturisticos);
 
 
 /* Aqui será finalizado apresentando os dados da Cidade*/
+printf("\n");
 printf("Parabéns, as propriedades da cidade %s foram criadas com sucesso!\n", cidade1);
+
+printf("Código da Carta: %s01\n", estado);
+printf("Cidade: %s\n",cidade1); 
+printf("População: %d\n",pop); 
+printf("Area: %f\n",area); 
+printf("PIB: %f\n",pib); 
+printf("PIB: %d\n",pontosturisticos); 
+
+/*
 printf("Cidade: %s - População: %d - Area: %f\n", cidade1, pop,area);
 printf("PIB: %f - Pontos Turísticos: %d\n", pib, pontosturisticos);
-
+*/
     return 0;
 }
